8c89ed13cc282177dd1c78dc13ee9edc90ca5eba
ranjeet-pivotchain <you@example.com>
2023-03-23 11:55:29 +0530

    next commit

--- install-k8s-componants.sh
+++ install-k8s-componants.sh
@@ -1,5 +1,6 @@
 echo "Installing k8s componants"
 k8sversion=1.24.3-00
+#k8sversion=1.24.3-00
 apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
